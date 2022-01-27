af247fac2e39656cd4bce7dda2ec57940b167826
Tony G. Bolaño <tonybolano@gmail.com>
2022-01-27 17:50:59 +0100

    feat: basic header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { StickyNote2 } from "@mui/icons-material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+export default function Header() {
+  return (
+    <header>
+      <AppBar position="static">
+        <Toolbar>
+          <StickyNote2 sx={{ mr: 2 }} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            React Notes
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </header >
+  )
+}
